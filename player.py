6b0c2c4e74290ad8fd161ed40d7e87b28a2a5943
liyang <ls3817@nyu.edu>
2019-11-24 20:24:49 -0500

    testing run

--- player.py
+++ player.py
@@ -14,7 +14,7 @@ MS_IN_S = 1000.0    # in ms
 KB_IN_MB = 1000.0   # in ms
 
 class Live_Player(object):
-    def __init__(self, throughput_trace, time_trace, trace_name, random_seed):
+    def __init__(self, throughput_trace, time_trace, trace_name, random_seed=Config.random_seed):
         np.random.seed(random_seed)
         self.throughput_trace = throughput_trace
         self.time_trace = time_trace
